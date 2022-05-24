6a495137190745c9a380ecb444c711dad8ac1e7d
Filipe de Oliveira Ataíde <filipeoliveirataide@gmail.com>
2022-05-24 19:47:39 -0300

    Corrigindo cabecalho no Layout 1.

--- scripts/c3runtime.js
+++ scripts/c3runtime.js
@@ -3855,7 +3855,7 @@ self.C3_ExpressionFuncs = [
 		},
 		p => {
 			const f0 = p._GetNode(0).GetBoundMethod();
-			return () => f0(280, 1030);
+			return () => f0(350, 980);
 		},
 		() => "",
 		() => 3,
